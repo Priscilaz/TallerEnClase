ae320296ba8d4fd45b7b30feb2ce807d6290cc89
Priscila Estefania Zuñiga Andino <priscila.zuniga@udla.edu.ec>
2024-04-30 22:38:12 -0500

    Se crea la clase Burger.cs y sus atributos usando Data Annotations

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -1,6 +1,16 @@
-﻿namespace PriscilaZuniga_Web_CodeFirst.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriscilaZuniga_Web_CodeFirst.Models
 {
     public class Burger
     {
+        public int BurgerID { get; set; }
+        // ? Especifica que ese atributo puede contener valores nulos
+        //[Required es un data annotation que se aplica solo a la linea de abajo]
+        [Required (ErrorMessage="El campo nombre es requerido")]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set;}
+        [Range(0.01, 9999.99, ErrorMessage ="Valor fuera de rango")]
+        public decimal Precio { get; set; }
     }
 }
